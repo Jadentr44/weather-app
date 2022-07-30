e5fd8872b4d505308acacf9339a47256f427ae99
unknown <jadentr.44@gmail.com>
2022-07-29 20:01:42 -0500

    we can get and display infor on current day

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,9 +12,37 @@ fetch(geoSearchAPI)
 .then(data=>{
   lat = data[0].lat
   lon = data[0].lon
-  console.log(`lat(${data[0].lat}) and lon(${data[0].lon})`)
+  $('#location').text(data[0].name)
+  let weatherApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=3eafd41a5ac2edac126aa9adf492b047`
+
+  fetch(weatherApi)
+  .then(response => response.json())
+  .then(data=>{
+    console.log(data)
+    displayInfo(data)
+  }).catch(function(){
+    console.log("err")
+  })
 }).catch(function() {
   console.log("error");
 });
 
 })
+
+
+
+function displayInfo(data){
+  let currentDay =data.daily[0]
+  let currentDate = new Date()
+  $('#day').text(`(${currentDate.getMonth()}/${currentDate.getDate()})`)
+  $('#temp').text(`temp:${data.current.temp}`)
+  $('#wind').text(`wind:${data.current.wind_speed}`)
+  $('#humidity').text(`humidity${data.current.humidity}`)
+
+  let fiveDay = [data.daily[0],data.daily[1],data.daily[2],data.daily[3],data.daily[4]]
+
+  fiveDay.forEach(e=>{
+    let container = $('div')
+    container.addClass('card col-2')
+  })
+}
